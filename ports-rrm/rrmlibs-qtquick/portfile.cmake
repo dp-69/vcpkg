3de8462202f3e40458c52ea94de68d7d7bb1e613
Dmytro Petrovskyy <dmytro.petrovsky@gmail.com>
2022-12-20 16:34:10 +0200

    rrmlibs-qtquick (added) | rrmlibs-legacy (renamed)

---
+++ ports-rrm/rrmlibs-qtquick/portfile.cmake
@@ -0,0 +1,7 @@
+SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+message(STATUS "")
+message(STATUS "")
+message(STATUS "Congratulations, libraries for RRM QtQuick are successfully installed")
+message(STATUS "")
+message(STATUS "")
